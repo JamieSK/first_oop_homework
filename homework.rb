f0a1947e869dc1e8243994db31fe63a16d10161c
Jamie Klein <jamiesklein@gmail.com>
2017-09-25 18:35:06 +0100

    Initial commit.

---
+++ homework.rb
@@ -0,0 +1,129 @@
+class Student
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
+
+  def name
+    @name
+  end
+
+  def cohort
+    @cohort
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+  def cohort=(new_cohort)
+    @cohort = new_cohort
+  end
+
+  def talk
+    'I can talk!'
+  end
+
+  def say_favourite_language(language)
+    "I love #{language}"
+  end
+end
+
+class SportsTeam
+  def initialize(name, players, coach)
+    @name = name
+    @players = players
+    @coach = coach
+    @points = 0
+  end
+
+  def name
+    @name
+  end
+
+  def players
+    @players
+  end
+
+  def coach
+    @coach
+  end
+
+  def points
+    @points
+  end
+
+  def coach=(new_coach)
+    @coach = new_coach
+  end
+
+  def add_player!(new_player)
+    @players << new_player
+  end
+
+  def player?(player)
+    @players.include?(player)
+  end
+
+  def update_points!(result)
+    @points += 3 if result == 'win'
+  end
+end
+
+class Library
+  def initialize
+    @books = [
+      {
+        title: 'lord_of_the_rings',
+        rental_details: {
+          student_name: 'Jeff',
+          date: '01/12/16'
+        }
+      },
+      {
+        title: 'consider_phlebas',
+        rental_details: {
+          student_name: 'Jamie',
+          date: '23/09/2018'
+        }
+      },
+      {
+        title: 'meditations',
+        rental_details: {
+          student_name: 'Jeff',
+          date: '01/12/16'
+        }
+      }
+    ]
+  end
+
+  def list_all_books
+    @books
+  end
+
+  def list(title)
+    @books.each do |book|
+      return book if book[:title] == title
+    end
+  end
+
+  def rental_details(title)
+    list(title)[:rental_details]
+  end
+
+  def add_book!(title)
+    @books << {
+      title: title,
+      rental_details: {
+        student_name: '',
+        date: ''
+      }
+    }
+  end
+
+  def change_renter!(title, renter, date)
+    book = list(title)
+    book[:rental_details][:student_name] = renter
+    book[:rental_details][:date] = date
+  end
+end
---
+++ homework_spec.rb
@@ -0,0 +1,162 @@
+require 'minitest/autorun'
+require 'minitest/rg'
+require_relative 'homework'
+
+class TestStudent < MiniTest::Test
+  def setup
+    @student = Student.new('Jamie', 3)
+  end
+
+  def test_get_name
+    assert_equal('Jamie', @student.name)
+  end
+
+  def test_get_cohort
+    assert_equal(3, @student.cohort)
+  end
+
+  def test_set_name
+    @student.name = 'Jim'
+    assert_equal('Jim', @student.name)
+  end
+
+  def test_set_cohort
+    @student.cohort = -4
+    assert_equal(-4, @student.cohort)
+  end
+
+  def test_talk
+    assert_equal('I can talk!', @student.talk)
+  end
+
+  def test_say_fav_language
+    assert_equal('I love Ruby', @student.say_favourite_language('Ruby'))
+  end
+end
+
+class TestSportsTeam < MiniTest::Test
+  def setup
+    @sports_team = SportsTeam.new(
+      'Glasgow Warriors',
+      ['Prop', 'Hooker', 'Prop', 'The rest'],
+      'Dave Rennie'
+    )
+  end
+
+  def test_get_team_name
+    assert_equal('Glasgow Warriors', @sports_team.name)
+  end
+
+  def test_get_players
+    assert_equal(['Prop', 'Hooker', 'Prop', 'The rest'], @sports_team.players)
+  end
+
+  def test_get_coach
+    assert_equal('Dave Rennie', @sports_team.coach)
+  end
+
+  def test_set_coach
+    @sports_team.coach = 'Neil Lennon'
+    assert_equal('Neil Lennon', @sports_team.coach)
+  end
+
+  def test_push_new_player
+    @sports_team.add_player!('LeBron James')
+    assert_equal(true, @sports_team.players.include?('LeBron James'))
+  end
+
+  def test_is_player__true
+    assert_equal(true, @sports_team.player?('Hooker'))
+  end
+
+  def test_is_player__false
+    assert_equal(false, @sports_team.player?('Batsman'))
+  end
+
+  def test_update_points__win
+    @sports_team.update_points!('win')
+    assert_equal(3, @sports_team.points)
+  end
+
+  def test_update_points__lose
+    @sports_team.update_points!('lose')
+    assert_equal(0, @sports_team.points)
+  end
+end
+
+class TestLibrary < MiniTest::Test
+  def setup
+    @library = Library.new
+  end
+
+  def test_list_all_books
+    result = [
+      {
+        title: 'lord_of_the_rings',
+        rental_details: {
+          student_name: 'Jeff',
+          date: '01/12/16'
+        }
+      },
+      {
+        title: 'consider_phlebas',
+        rental_details: {
+          student_name: 'Jamie',
+          date: '23/09/2018'
+        }
+      },
+      {
+        title: 'meditations',
+        rental_details: {
+          student_name: 'Jeff',
+          date: '01/12/16'
+        }
+      }
+    ]
+    assert_equal(result, @library.list_all_books)
+  end
+
+  def test_list_one_book
+    result = {
+      title: 'consider_phlebas',
+      rental_details: {
+        student_name: 'Jamie',
+        date: '23/09/2018'
+      }
+    }
+    assert_equal(result, @library.list('consider_phlebas'))
+  end
+
+  def test_rental_details
+    result = {
+      student_name: 'Jamie',
+      date: '23/09/2018'
+    }
+    assert_equal(result, @library.rental_details('consider_phlebas'))
+  end
+
+  def test_add_book
+    @library.add_book!('the_dictionary')
+    result = {
+      title: 'the_dictionary',
+      rental_details: {
+        student_name: '',
+        date: ''
+      }
+    }
+    assert_equal(result, @library.list('the_dictionary'))
+  end
+
+  def test_change_renter
+    @library.change_renter!('lord_of_the_rings', 'Jamie', '23/09/2018')
+    actual = @library.list('lord_of_the_rings')
+    expected = {
+      title: 'lord_of_the_rings',
+      rental_details: {
+        student_name: 'Jamie',
+        date: '23/09/2018'
+      }
+    }
+    assert_equal(actual, expected)
+  end
+end
